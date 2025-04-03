257969fd136a02850c3df5de1896380c048e570b
k971119 <hanyul97@naver.com>
2025-04-03 16:18:00 +0900

    [ci💚] code deploy 스크립트 파일 수정
    - 의미없는 ecr pull 명령 삭제
    - 경로변경
    - 주석추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,8 @@
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/Spoteditor-Backend
+cd /home/spoteditor/Spoteditor-Backend
+
+echo "--------------- 기존 도커 컨테이너 다운 -----------------"
 docker compose down || true
-docker pull 891612578477.dkr.ecr.ap-northeast-2.amazonaws.com/spoteditor:latest
+echo "--------------- 도커 컴포즈 이미지 업 -----------------"
 docker compose up -d --build
 echo "--------------- 서버 배포 끝 -----------------"
