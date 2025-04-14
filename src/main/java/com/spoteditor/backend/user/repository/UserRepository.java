371751f6d5c7251eb755ca82ecdcb6a097efa5c9
jwj <dnwls16071@naver.com>
2025-04-15 02:25:27 +0900

    [refactor♻️] 유저 리포지토리 리팩토링(#159)
    - 불필요한 메서드 삭제 후 이메일로 조회할 수 있는 메서드 추가

--- src/main/java/com/spoteditor/backend/user/repository/UserRepository.java
+++ src/main/java/com/spoteditor/backend/user/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.spoteditor.backend.user.repository;
 
 import com.spoteditor.backend.user.entity.User;
-import com.spoteditor.backend.user.entity.OauthProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByOauthUserIdAndProvider(String oauthUserId, OauthProvider provider);
 
-    Optional<User> findUserByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
