51ba3caffafa39db94b2a40d26f85cd7e9c22860
k971119 <hanyul97@naver.com>
2025-05-05 16:11:34 +0900

    [refactor♻️] PlaceLogPopularityService측 중복및 미사용 코드 정리

--- src/main/java/com/spoteditor/backend/modules/placelog/service/PlaceLogPopularityService.java
+++ src/main/java/com/spoteditor/backend/modules/placelog/service/PlaceLogPopularityService.java
@@ -17,7 +17,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PlaceLogPopularityService {
 
-    private final PlaceLogRepository placeRepository;
     private final PlaceLogRepository placeLogRepository;
 
     private final PlaceLogPopularityRedisRepository placeLogPopularityRedisRepository;
@@ -51,41 +50,11 @@ public class PlaceLogPopularityService {
         return this.calculatePopulateScore(view, bookmark, date, 1.5f);
     }
 
-    // 다시 사용할 여지가 있어 지우지 않고 남겨둠
-    /**
-     * 전체 로그 게시물의 인기도를 산정하여 업데이트
-     * @param loadPerLogCount   한번에 처리할 로그 게시물 갯수(페이징)
-     */
-    @Transactional
-    public void updateAllLogPopularity(int loadPerLogCount){
-        int page = 0;
-        int pageSize = loadPerLogCount;
-
-        while(true){
-            int offset = page * pageSize;
-            List<PlaceLogWithBookmark> placeLogWithBookmarks = placeRepository.findPlaceLogWithBookmarkCount(pageSize, offset);
-
-            if(placeLogWithBookmarks.isEmpty()){
-                break;
-            }
-
-            for(PlaceLogWithBookmark placeLogWithBookmark : placeLogWithBookmarks){
-                float popularityScore = calculatePopulateScore(placeLogWithBookmark.viewCount(), placeLogWithBookmark.bookmarkCount(), placeLogWithBookmark.createAt());
-
-                PlaceLog placeLog = placeLogRepository.findById(placeLogWithBookmark.placeLogId()).orElseThrow(
-                        () -> new RuntimeException("인기도 업데이트중 찾을 수 없는 로그를 발견했습니다. ID = " + placeLogWithBookmark.placeLogId())
-                );
-                //placeLog.setPopularityScore(popularityScore);     // 해당컬럼 삭제됨
-            }
-            page++;
-        }
-    }
-
     @Async
     public void updatePopularityScoreOnRedis(){
 
         // 게시글 전체 조회
-        List<PlaceLogWithBookmark> placeLogWithBookmarks = placeRepository.findPlaceLogWithBookmarkCount(Integer.MAX_VALUE, 0);
+        List<PlaceLogWithBookmark> placeLogWithBookmarks = placeLogRepository.findPlaceLogWithBookmarkCount(Integer.MAX_VALUE, 0);
 
         placeLogWithBookmarks.forEach(l -> {
             float popularityScore = calculatePopulateScore(l.viewCount(), l.bookmarkCount(), l.createAt());
